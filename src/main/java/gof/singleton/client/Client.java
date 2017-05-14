8cd86e6ef0dab02ef4d7a285d106c745942a4853
Keid <kssk96@gmail.com>
2017-05-14 15:10:56 +0900

    Create Singleton pattern's example.

---
+++ src/main/java/gof/singleton/client/Client.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2017 Keid
+*/
+package gof.singleton.client;
+
+import gof.singleton.product.NormalSingleton;
+import gof.singleton.product.RegistrySingleton;
+
+/**
+ * Client for Singleton pattern.
+ * 
+ * @author Keid
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+        NormalSingleton obj1 = NormalSingleton.getInstance();
+        NormalSingleton obj2 = NormalSingleton.getInstance();
+        if (obj1 == obj2) {
+            obj1.display();
+        }
+
+        RegistrySingleton obj3 = RegistrySingleton.getInstance("One");
+        RegistrySingleton obj4 = RegistrySingleton.getInstance("One");
+        RegistrySingleton obj5 = RegistrySingleton.getInstance("Two");
+        RegistrySingleton obj6 = RegistrySingleton.getInstance("Two");
+        System.out.println("Compare " + obj3.getName() + " to " + obj4.getName() + ".");
+        if (obj3 == obj4) {
+            obj3.display();
+        } else {
+            System.out.println("Not equal.");
+        }
+        System.out.println("Compare " + obj4.getName() + " to " + obj5.getName() + ".");
+        if (obj3 == obj5) {
+            obj3.display();
+        } else {
+            System.out.println("Not equal.");
+        }
+        System.out.println("Compare " + obj5.getName() + " to " + obj6.getName() + ".");
+        if (obj5 == obj6) {
+            obj5.display();
+        } else {
+            System.out.println("Not equal.");
+        }
+
+    }
+
+}
